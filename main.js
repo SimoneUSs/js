cc4ca4559d1f5f84c30f96525d46d0ac27e44627
SimoneUSs <simoneussia0@gmail.com>
2026-07-02 10:33:03 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+let num1 = 45;
+let num2 = 15;
+
+// il valore della variabile num1 è 45
+// il valore della variabile num2 è 15
+
+console.log("Il valore della variabile num1 è: " + num1);
+console.log(`Il valore della variabile num2 è ${num2}`);
+
+
+let stringa = "Ciao a tutti";
+
+
+stringa = "Benvenuti";
+console.log(`Il valore della variabile stringa è ${stringa}`);
+
+const pGreco = 3.14;
+
+
+
+pGreco = 100;
+console.log(`Il valore della variabile pGreco è ${pGreco}`);
